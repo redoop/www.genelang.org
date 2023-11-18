da132e537de680ea1cc2dfd156a42c28c45f49be
tongxiaojun <tongxiaojun@redoop.com>
2023-11-18 16:20:35 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="http://cloud.redoop.com">
             快速开始
           </Link>
         </div>
