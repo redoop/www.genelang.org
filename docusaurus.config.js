46a9541e2fcbb6cb5b82973f0b791c29f4022d43
tongxiaojun <tongxiaojun@redoop.com>
2024-03-17 15:26:50 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Genelang Community',
-  tagline: 'a openSource community for innovative biotechnology!',
+  tagline: 'a opensource community for innovative biotechnology!',
   url: 'http://wwww.genelang.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -36,13 +36,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/redoop/community.redoop.com/tree/main/',
+          editUrl: 'https://github.com/redoop/www.genelang.org/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/redoop/community.redoop.com/tree/main/',
+          editUrl: 'https://github.com/redoop/www.genelang.org/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
